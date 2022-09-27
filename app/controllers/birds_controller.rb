e899e8408127d8dd6510a1568e94174a9ac406a9
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-27 16:19:23 +0300

    commit tree ed4d3561a35bf2e46227fdb5a811066803fb7687

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,15 +1,12 @@
 class BirdsController < ApplicationController
+  # disable wrap parameters feature
+  wrap_parameters format: []
 
+  
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
-  end
-
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
   end
 
   # GET /birds/:id
@@ -21,5 +18,23 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  # POST /birds
+  def create
+    # bird = Bird.create(name: params[:name], species: params[:species])
+
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  # handling strong params 
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
+  
 
 end
